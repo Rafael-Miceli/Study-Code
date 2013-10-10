3a7da76bca526be3303601cba0cb2ed9b9186bbe
Rafael Miceli <rafael.miceli@hotmail.com>
2013-10-10 20:33:49 -0300

    Creating The problem To Solve with Facade

--- SlnStudy-Code/Kata 3/FacadePatternStudy/ConsoleAppFacadePatternStudy/Program.cs
+++ SlnStudy-Code/Kata 3/FacadePatternStudy/ConsoleAppFacadePatternStudy/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleAppFacadePatternStudy
     {
         static void Main(string[] args)
         {
+            var associateService = new AssociateService();
+            var associate = associateService.GetAssociate(2);
+
+            var itemsService = new itemsService();
+            var expenseList = new ExpenseList();
+            expenseList.Add(itemsService.GetLast5BuyedItems(associate));
+
+            var turnStilePoolService = new TurnStileService(PoolTurnStile);
+            var turnStileTenisService = new TurnStileService(TenisTrunStile);
+
+            expenseList.Add(turnStilePoolService.GetLast2Turned(associate));
+            expenseList.Add(turnStileTenisService.GetLast2Turned(associate));
+
+            foreach (var expense in expenseList)
+            {
+                Console.WriteLine(expense.Name);    
+            }
+
+            Console.ReadKey();
         }
     }
 }
