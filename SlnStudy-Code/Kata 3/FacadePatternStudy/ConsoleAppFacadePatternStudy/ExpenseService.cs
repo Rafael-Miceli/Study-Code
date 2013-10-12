2a0c10e7b0c2621c9a93f9c64415eed6d166b17b
Rafael Miceli <rafael.miceli@hotmail.com>
2013-10-12 12:23:29 -0300

    Kata 3 - Completed the facade pattern study

---
+++ SlnStudy-Code/Kata 3/FacadePatternStudy/ConsoleAppFacadePatternStudy/ExpenseService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleAppFacadePatternStudy
+{
+    //This is a "facade" class
+    public class ExpenseService
+    {
+        public IEnumerable<IExpense> GetAssociateExpenses(Associate associate)
+        {
+            var itemsService = new ItemsService();
+            var turnStilePoolService = new TurnStyleService(new PoolTurnStyle());
+            var turnStileTenisService = new TurnStyleService(new TenisTurnStyle());
+
+            var expenseList = new List<IExpense>
+                {
+                    itemsService.GetLast5BuyedItems(associate),
+                    turnStilePoolService.GetLast2Turned(associate),
+                    turnStileTenisService.GetLast2Turned(associate)
+                };
+
+            return expenseList;
+        }
+    }
+}
--- SlnStudy-Code/Kata 3/FacadePatternStudy/ConsoleAppFacadePatternStudy/Program.cs
+++ SlnStudy-Code/Kata 3/FacadePatternStudy/ConsoleAppFacadePatternStudy/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAppFacadePatternStudy
 {
@@ -13,17 +9,8 @@ namespace ConsoleAppFacadePatternStudy
             var associateService = new AssociateService();
             var associate = associateService.GetAssociate(2);
 
-            var itemsService = new ItemsService();
-            var expenseList = new List<IExpense>
-                {
-                    itemsService.GetLast5BuyedItems(associate)
-                };
-
-            var turnStilePoolService = new TurnStyleService(new PoolTurnStyle());
-            var turnStileTenisService = new TurnStyleService(new TenisTurnStyle());
-
-            expenseList.Add(turnStilePoolService.GetLast2Turned(associate));
-            expenseList.Add(turnStileTenisService.GetLast2Turned(associate));
+            var expenseService = new ExpenseService();
+            var expenseList = expenseService.GetAssociateExpenses(associate);
 
             foreach (var expense in expenseList)
             {
