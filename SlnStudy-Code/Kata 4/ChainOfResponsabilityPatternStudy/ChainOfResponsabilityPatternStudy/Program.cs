61c4004296f548dbf7eb46bd2f012c966825dce0
Rafael Miceli <rafael.miceli@hotmail.com>
2013-11-05 13:31:18 -0200

    Creating a problem to solve with CoR

--- SlnStudy-Code/Kata 4/ChainOfResponsabilityPatternStudy/ChainOfResponsabilityPatternStudy/Program.cs
+++ SlnStudy-Code/Kata 4/ChainOfResponsabilityPatternStudy/ChainOfResponsabilityPatternStudy/Program.cs
@@ -9,6 +9,34 @@ namespace ChainOfResponsabilityPatternStudy
     {
         static void Main(string[] args)
         {
+
         }
+    }
+
+
+
+    public class Ticket
+    {
+        public TicketStatus TicketStatus { get; set; }
+        public string Title { get; set; }
+        public Person Person { get; set; }
+    }
+
+    public enum TicketStatus
+    {
+        Analyse,
+        Attend,
+        Approving,
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public class Role
+    {
+        public string Name { get; set; }
     }
 }
