7622598035e08477e1e267622d6972cb61b0939a
Rafael Miceli <rafael.miceli@hotmail.com>
2013-11-06 13:24:47 -0200

    finalized CoR implementation

--- SlnStudy-Code/Kata 4/ChainOfResponsabilityPatternStudy/ChainOfResponsabilityPatternStudy/Program.cs
+++ SlnStudy-Code/Kata 4/ChainOfResponsabilityPatternStudy/ChainOfResponsabilityPatternStudy/Program.cs
@@ -9,13 +9,39 @@ namespace ChainOfResponsabilityPatternStudy
     {
         static void Main(string[] args)
         {
-            //No  CoR
+            
 
-            /*
+            
             var ticket1 = new Ticket("Ticket 1");
             var ticket2 = new Ticket("Ticket 2", TicketStatus.WaitingPieces);
             var ticket3 = new Ticket("Ticket 3", TicketStatus.Resolved);
 
+            var tickets = new List<Ticket>
+                {
+                    ticket1,
+                    ticket2,
+                    ticket3
+                };
+
+            #region "With CoR"
+
+            var celso = new TicketStatusHandler(new Person("Celso", "Tec 3", TicketStatus.Open));
+            var talita = new TicketStatusHandler(new Person("Talita", "Atendente", TicketStatus.Resolved));
+            var mike = new TicketStatusHandler(new Person("Mike", "Tec 1", TicketStatus.Open));
+            var tino = new TicketStatusHandler(new Person("Tino", "Administrador", TicketStatus.WaitingPieces));
+            
+            mike.RegisterNext(celso);
+            celso.RegisterNext(tino);
+            tino.RegisterNext(talita);
+
+
+            foreach (var ticket in tickets)
+                Console.WriteLine(mike.ResolveTicket(ticket).Name + " Resolved the problem of " + ticket.Title);
+
+            #endregion
+
+            #region "Without CoR"
+            /*
             var celso = new Person("Celso", "Tec 3", TicketStatus.Open);
             var talita = new Person("Talita", "Atendente", TicketStatus.Resolved);
             var mike = new Person("Mike", "Tec 1", TicketStatus.Open);
@@ -27,13 +53,7 @@ namespace ChainOfResponsabilityPatternStudy
                     talita,
                     mike,
                     tino
-                };
-            var tickets = new List<Ticket>
-                {
-                    ticket1,
-                    ticket2,
-                    ticket3
-                };
+                };         
 
             foreach (var ticket in tickets)
             {
@@ -47,11 +67,9 @@ namespace ChainOfResponsabilityPatternStudy
             }
              * 
              */
+            #endregion
 
-
-            //With Cor
-
-
+            Console.Read();
         }
     }
 }
