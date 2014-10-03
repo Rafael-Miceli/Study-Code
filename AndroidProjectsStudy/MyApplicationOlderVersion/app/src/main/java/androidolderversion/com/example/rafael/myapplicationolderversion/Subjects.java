79c25681daeb721d03f52a910de2792a8d36a913
Rafael Miceli <rafael.miceli@hotmail.com>
2014-10-03 19:18:04 -0300

    New Activity in grades app for android

--- AndroidProjectsStudy/MyApplicationOlderVersion/app/src/main/java/androidolderversion/com/example/rafael/myapplicationolderversion/Subjects.java
+++ AndroidProjectsStudy/MyApplicationOlderVersion/app/src/main/java/androidolderversion/com/example/rafael/myapplicationolderversion/Subjects.java
@@ -1,26 +1,24 @@
 package androidolderversion.com.example.rafael.myapplicationolderversion;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 
-public class MyActivity extends Activity {
+public class Subjects extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_subjects);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.my_activity2, menu);
         return true;
     }
 
@@ -30,25 +28,9 @@ public class MyActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_grades) {
-            onMenuItemGradeTouch();
+        if (id == R.id.action_settings) {
             return true;
         }
-        if (id == R.id.action_subjects) {
-            onMenuItemSubjectTouch();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onMenuItemSubjectTouch(){
-        Intent intent = new Intent(this, Subjects.class);
-        startActivity(intent);
-    }
-
-    public void onMenuItemGradeTouch(){
-        Toast toast = Toast.makeText(this, "Indo para notas", Toast.LENGTH_LONG);
-        toast.show();
     }
 }
