650b337b91189b465aacf82725f2eacb8843b048
Rafael Miceli <rafael.miceli@hotmail.com>
2014-10-23 16:29:04 -0200

    Configured Facebook login in android project

--- AndroidProjectsStudy/ApplicationOAuthFaceBook/app/src/main/java/oauthfacebook/com/example/rafaelmiceli/applicationoauthfacebook/FacebookLogin.java
+++ AndroidProjectsStudy/ApplicationOAuthFaceBook/app/src/main/java/oauthfacebook/com/example/rafaelmiceli/applicationoauthfacebook/FacebookLogin.java
@@ -1,9 +1,25 @@
 package oauthfacebook.com.example.rafaelmiceli.applicationoauthfacebook;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class FacebookLogin extends Activity {
@@ -12,25 +28,34 @@ public class FacebookLogin extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_login);
+
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                if (session.isOpened()) {
+                    // make request to the /me API
+                    Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                        // callback after Graph API response with user object
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            if (user != null) {
+                                Toast.makeText(FacebookLogin.this,  "Hello " + user.getName() + "!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }).executeAsync();
+                }
+            }
+        });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.facebook_login, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
 }
