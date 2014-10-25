2fbef45fbb07c137bf317ce03be942bd5b4fbbd9
Rafael Miceli <rafael.miceli@hotmail.com>
2014-10-25 21:33:38 -0200

    Facebook login created, but is breaking

--- AndroidProjectsStudy/ApplicationOAuthFaceBook/app/src/main/java/oauthfacebook/com/example/rafaelmiceli/applicationoauthfacebook/FacebookLogin.java
+++ AndroidProjectsStudy/ApplicationOAuthFaceBook/app/src/main/java/oauthfacebook/com/example/rafaelmiceli/applicationoauthfacebook/FacebookLogin.java
@@ -1,61 +1,174 @@
 package oauthfacebook.com.example.rafaelmiceli.applicationoauthfacebook;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.facebook.Request;
-import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
-import com.facebook.model.GraphUser;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.facebook.UiLifecycleHelper;
 
 
-public class FacebookLogin extends Activity {
+public class FacebookLogin extends FragmentActivity {
+
+    private static final int SPLASH = 0;
+    private static final int MAIN = 1;
+    private static final int SETTINGS = 2;
+    private static final int FRAGMENT_COUNT = SETTINGS + 1;
+
+
+    private MenuItem settings;
+    private boolean isResumed = false;
+    private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+    private UiLifecycleHelper uiHelper;
+    private Session.StatusCallback callback =
+            new Session.StatusCallback() {
+                @Override
+                public void call(Session session,
+                                 SessionState state, Exception exception) {
+                    onSessionStateChange(session, state, exception);
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_facebook_login);
 
-        // start Facebook Login
-        Session.openActiveSession(this, true, new Session.StatusCallback() {
+        FragmentManager fm = getSupportFragmentManager();
+        fragments[SPLASH] = fm.findFragmentById(R.id.splashFragment);
+        fragments[MAIN] = fm.findFragmentById(R.id.mainFragment);
+        fragments[SETTINGS] = fm.findFragmentById(R.id.userSettingsFragment);
 
-            // callback when session changes state
-            @Override
-            public void call(Session session, SessionState state, Exception exception) {
-                if (session.isOpened()) {
-                    // make request to the /me API
-                    Request.newMeRequest(session, new Request.GraphUserCallback() {
 
-                        // callback after Graph API response with user object
-                        @Override
-                        public void onCompleted(GraphUser user, Response response) {
-                            if (user != null) {
-                                Toast.makeText(FacebookLogin.this,  "Hello " + user.getName() + "!", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    }).executeAsync();
-                }
+
+        FragmentTransaction transaction = fm.beginTransaction();
+        for(int i = 0; i < fragments.length; i++) {
+            transaction.hide(fragments[i]);
+        }
+        transaction.commit();
+
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        // Only make changes if the activity is visible
+        if (isResumed) {
+            FragmentManager manager = getSupportFragmentManager();
+            // Get the number of entries in the back stack
+            int backStackSize = manager.getBackStackEntryCount();
+            // Clear the back stack
+            for (int i = 0; i < backStackSize; i++) {
+                manager.popBackStack();
             }
-        });
+
+            if (state.isOpened()) {
+                // If the session state is open:
+                // Show the authenticated fragment
+                showFragment(MAIN, false);
+            } else if (state.isClosed()) {
+                // If the session state is closed:
+                // Show the login fragment
+                showFragment(SPLASH, false);
+            }
+        }
+    }
+
+    private void showFragment(int fragmentIndex, boolean addToBackStack) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        for (int i = 0; i < fragments.length; i++) {
+            if (i == fragmentIndex) {
+                transaction.show(fragments[i]);
+            } else {
+                transaction.hide(fragments[i]);
+            }
+        }
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // only add the menu when the selection fragment is showing
+        if (fragments[MAIN].isVisible()) {
+            if (menu.size() == 0) {
+                settings = menu.add(R.string.settings);
+            }
+            return true;
+        } else {
+            menu.clear();
+            settings = null;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.equals(settings)) {
+            showFragment(SETTINGS, true);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        Session session = Session.getActiveSession();
+
+        if (session != null && session.isOpened()) {
+            // if the session is already open,
+            // try to show the selection fragment
+            showFragment(MAIN, false);
+        } else {
+            // otherwise present the splash screen
+            // and ask the person to login.
+            showFragment(SPLASH, false);
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+        isResumed = true;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+        isResumed = false;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
 }
