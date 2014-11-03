124d2350fc1bc780bdc03baab164e27617c91a23
Rafael Miceli <rafael.miceli@hotmail.com>
2014-11-03 08:11:24 -0200

    Commit

--- AndroidProjectsStudy/ApplicationOAuthFaceBook/app/src/main/java/oauthfacebook/com/example/rafaelmiceli/applicationoauthfacebook/MyProfile.java
+++ AndroidProjectsStudy/ApplicationOAuthFaceBook/app/src/main/java/oauthfacebook/com/example/rafaelmiceli/applicationoauthfacebook/MyProfile.java
@@ -23,14 +23,6 @@ public class MyProfile extends Activity implements ListView.OnItemClickListener 
         mNavigationDrawerHelper.init(this, this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.my_profile, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
